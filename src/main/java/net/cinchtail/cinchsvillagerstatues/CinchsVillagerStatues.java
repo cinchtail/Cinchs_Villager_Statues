fed20f2ccd862dc2cb817c7621a488746da37b3c
Joshua Davies <cinchtail@gmail.com>
2025-06-14 01:31:08 -0400

    all added

---
+++ src/main/java/net/cinchtail/cinchsvillagerstatues/CinchsVillagerStatues.java
@@ -0,0 +1,23 @@
+package net.cinchtail.cinchsvillagerstatues;
+
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(CinchsVillagerStatues.MODID)
+public class CinchsVillagerStatues {
+    public static final String MODID = "cinchsvillagerstatues";
+
+    public CinchsVillagerStatues() {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+    }
+}
