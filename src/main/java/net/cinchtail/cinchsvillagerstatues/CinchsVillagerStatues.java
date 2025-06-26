db7e6d7b13a7bd6e0480291c67be6544af5ecb1a
Joshua Davies <cinchtail@gmail.com>
2025-06-26 17:50:04 -0400

    all added

---
+++ src/main/java/net/cinchtail/cinchsvillagerstatues/CinchsVillagerStatues.java
@@ -0,0 +1,12 @@
+package net.cinchtail.cinchsvillagerstatues;
+
+import org.slf4j.Logger;
+
+import net.neoforged.fml.common.Mod;
+import org.slf4j.LoggerFactory;
+
+@Mod(CinchsVillagerStatues.MOD_ID)
+public class CinchsVillagerStatues {
+    public static final String MOD_ID = "cinchsvillagerstatues";
+    public static final Logger LOGGER = LoggerFactory.getLogger(CinchsVillagerStatues.class);
+}
