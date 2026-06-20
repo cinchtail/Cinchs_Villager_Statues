d600da19abd9a41e905787f45547981289f1a3ca
Joshua Davies <cinchtail@gmail.com>
2026-06-20 17:59:55 -0400

    fixes and loot table changes

--- src/main/java/net/cinchtail/cinchsvillagerstatues/CinchsVillagerStatues.java
+++ src/main/java/net/cinchtail/cinchsvillagerstatues/CinchsVillagerStatues.java
@@ -1,11 +1,8 @@
 package net.cinchtail.cinchsvillagerstatues;
 
 import net.minecraftforge.fml.common.Mod;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Mod(CinchsVillagerStatues.MOD_ID)
 public class CinchsVillagerStatues {
     public static final String MOD_ID = "cinchsvillagerstatues";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 }
